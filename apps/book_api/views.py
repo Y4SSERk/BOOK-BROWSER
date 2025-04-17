0dd06c7854103fc75cfb94061192964298084cd2
YASSER <yasser.khattach@gmail.com>
2025-04-17 20:09:18 +0100

    setup templates

--- apps/book_api/views.py
+++ apps/book_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'pages/home.html')
+    # return HttpResponse("<h1>Welcome to the Book API</h1>")
+
+def browser(request):
+    return render(request, 'pages/browser.html')
+    # return HttpResponse("<h1>Welcome to the Book Browser</h1>")
+
+def map(request):
+    return render(request, 'pages/map.html')
+    # return HttpResponse("<h1>Welcome to the Book Map</h1>")
