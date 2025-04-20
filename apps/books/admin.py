346e2d9b046c393ba8ab81e27fb01229c173c09a
YASSER <yasser.khattach@gmail.com>
2025-04-20 15:31:00 +0100

    book model

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Book
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_authors', 'publisher', 'published_date', 'average_rating')
+    list_filter = ('genres', 'language', 'setting_country')
+    search_fields = ('title', 'authors', 'isbn', 'publisher')
+    filter_horizontal = ()
+    readonly_fields = ('cover_image_preview',)
+    fieldsets = (
+        ('Core Information', {
+            'fields': ('title', 'subtitle', 'authors', 'isbn')
+        }),
+        ('Publication Details', {
+            'fields': ('publisher', 'published_date', 'edition', 'page_count', 'language')
+        }),
+        ('Classification', {
+            'fields': ('genres', 'subjects', 'keywords', 'mood_tags', 'age_range')
+        }),
+        ('Geographic Data', {
+            'fields': ('author_countries', 'setting_country')
+        }),
+        ('Media & Links', {
+            'fields': ('cover_image', 'cover_image_preview', 'purchase_links', 'borrow_links')
+        }),
+        ('Ratings', {
+            'fields': ('average_rating',)
+        }),
+    )
+
+    def display_authors(self, obj):
+        return ", ".join(obj.authors)
+    display_authors.short_description = 'Authors'
+
+    def cover_image_preview(self, obj):
+        if obj.cover_image:
+            return mark_safe(f'<img src="{obj.cover_image}" style="max-height: 200px;" />')
+        return "No cover image"
+    cover_image_preview.short_description = 'Cover Preview'
+
+
+# Register your models
+admin.site.register(Book, BookAdmin)
--- apps/books/models.py
+++ apps/books/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.postgres.fields import ArrayField
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+User = get_user_model()
+
+class Book(models.Model):
+    # Core Identification
+    title = models.CharField(max_length=500)
+    subtitle = models.CharField(max_length=500, blank=True)
+    authors = ArrayField(models.CharField(max_length=200), default=list)
+    isbn = models.CharField(max_length=20, blank=True, unique=True)
+    
+    # Publication Info
+    publisher = models.CharField(max_length=200, blank=True)
+    published_date = models.DateField(null=True, blank=True)
+    edition = models.CharField(max_length=50, blank=True)
+    page_count = models.PositiveIntegerField(null=True, blank=True)
+    language = models.CharField(max_length=50, blank=True)
+    
+    # Content Classification
+    genres = ArrayField(models.CharField(max_length=100), default=list)
+    subjects = ArrayField(models.CharField(max_length=200), default=list)
+    keywords = ArrayField(models.CharField(max_length=100), default=list)
+    mood_tags = ArrayField(models.CharField(max_length=50), default=list)
+    age_range = models.CharField(max_length=20, blank=True)
+    
+    # Geographic Data
+    author_countries = ArrayField(models.CharField(max_length=100), default=list)
+    setting_country = models.CharField(max_length=100, blank=True)
+    
+    # Media & Links
+    cover_image = models.URLField(blank=True)
+    
+    # Purchase/Access Options
+    purchase_links = models.JSONField(default=dict)  # Changed to django.db.models.JSONField
+    borrow_links = models.JSONField(default=dict)    # Changed to django.db.models.JSONField
+    
+    # Ratings
+    average_rating = models.FloatField(
+        null=True, 
+        blank=True,
+        validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['title']),
+            models.Index(fields=['authors']),
+            models.Index(fields=['genres']),
+            models.Index(fields=['setting_country']),
+        ]
+        ordering = ['title']
+
+    def __str__(self):
+        return f"{self.title} by {', '.join(self.authors)}"
