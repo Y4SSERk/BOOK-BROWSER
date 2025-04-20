bc1e5d06afc2dc9d34ee6cb2b8f486a520231a20
YASSER <yasser.khattach@gmail.com>
2025-04-20 12:25:06 +0100

    connect Postgresql db

--- project/settings.py
+++ project/settings.py
@@ -73,14 +73,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BooksDB',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
